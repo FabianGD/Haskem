0539d9b97a1c349fd2d161f6439c7cc219f4523e
Fabian George Droege <fabian.droege@uni-jena.de>
2019-08-19 16:15:26 +0200

    Forgot the Parser.hs file

---
+++ src/Haskem/Parsers.hs
@@ -0,0 +1,263 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Haskem.Parsers(
+    xyzParser,
+    gaussianFreqParser,
+    gaussianParser,
+    elemNum2Sym,
+    elemSym2Num,
+    elemMass
+) where
+
+import              Haskem.Types
+import              Data.Attoparsec.Text.Lazy   hiding (take)
+import              Prelude                     hiding (takeWhile)
+import qualified    Data.Map.Strict             as Map
+import              Data.Maybe
+import              Data.Tuple.HT               hiding (double, triple)
+import              Debug.Trace
+import              Data.List                   hiding (takeWhile)
+
+
+
+elemSym2Num :: String -> Int
+elemSym2Num elemSym = fromMaybe 0 $ Map.lookup elemSym elemMap  
+    where elemMap = Map.fromList [("H", 1),     ("He", 2),  ("Li", 3), 
+                                  ("Be", 4),    ("B", 5),   ("C", 6),
+                                  ("N", 7),     ("O", 8),   ("F", 9), 
+                                  ("Ne", 10),   ("Na", 11), ("Mg", 12),
+                                  ("Al", 13),   ("Si", 14), ("P", 15),
+                                  ("S", 16),    ("Cl", 17), ("Ar", 18)]
+
+elemNum2Sym :: Int -> String
+elemNum2Sym elemSym = fromMaybe "" $ Map.lookup elemSym elemMap  
+    where elemMap = Map.fromList $ map swap [("H", 1),     ("He", 2),  ("Li", 3), 
+                                             ("Be", 4),    ("B", 5),   ("C", 6),
+                                             ("N", 7),     ("O", 8),   ("F", 9), 
+                                             ("Ne", 10),   ("Na", 11), ("Mg", 12),
+                                             ("Al", 13),   ("Si", 14), ("P", 15),
+                                             ("S", 16),    ("Cl", 17), ("Ar", 18)]
+                                            
+
+elemMass :: Int -> Double
+elemMass elemNum = fromMaybe 1 $ Map.lookup elemNum elemMap  
+    where elemMap = Map.fromList [(1, 1.0079),      (2, 4.0026),    (3, 6.941), 
+                                  (4, 9.0122),      (5, 10.811),    (6, 12.0107),
+                                  (7, 14.0067),     (8, 15.9994),   (9, 18.9984),
+                                  (10, 20.1797),    (11, 22.9897),  (12, 24.305),
+                                  (13, 26.9815),    (14, 28.0855),  (15, 30.9738),
+                                  (16, 32.065),     (17, 35.453),   (18, 39.948)]
+
+
+xyzParser :: Parser Molecule
+xyzParser = do
+    skipSpace
+    nrOfAtoms' <- decimal
+
+    _ <- takeWhile (not <$> isEndOfLine) *> endOfLine
+    _ <- takeWhile (not <$> isEndOfLine) *> endOfLine
+
+    atoms' <- count nrOfAtoms' $ do
+        skipSpace
+
+        elementSym' <- manyTill anyChar space
+        let elemNum' = elemSym2Num elementSym'
+
+        skipSpace
+        coordx' <- double <* skipSpace
+        coordy' <- double <* skipSpace
+        coordz' <- double
+
+        _ <- takeWhile (not <$> isEndOfLine) *> endOfLine
+
+        return Atom {
+            _centerNumber   = Nothing,
+            _atomNumber     = elemNum',
+            _coordinate     = CartesianCoord coordx' coordy' coordz'
+        }
+
+    return Molecule {
+        _molName    = "",
+        _nrOfAtoms  = nrOfAtoms',
+        _atoms      = atoms'
+    }
+
+gaussianParseXYZ :: Parser Atom
+gaussianParseXYZ = do
+    -- Skip inital whitespace
+    skipSpace
+
+    -- Center number is not required
+    centerNumber' <- decimal <* skipSpace
+
+    -- Atom (element) number
+    atomNumber' <- decimal <* skipSpace
+
+    -- Atomic Type is irrelevant
+    _ <- decimal <* skipSpace
+
+    -- Coordinates
+    coordx' <- double <* skipSpace
+    coordy' <- double <* skipSpace
+    coordz' <- double <* skipSpace
+
+    _ <- takeWhile (not <$> isEndOfLine) *> endOfLine
+
+    return Atom {
+            _centerNumber   = Just centerNumber',
+            _atomNumber     = atomNumber',
+            _coordinate     = CartesianCoord coordx' coordy' coordz'
+        }
+
+gaussianParser :: Parser GaussianInfo
+gaussianParser = do
+    -- TODO --> Read Internal coordinates
+
+    -- ==> Read XYZ section in Gaussian files
+    _ <- manyTill anyChar (string "Standard orientation")
+
+    _ <- count 5 $ do
+        _ <- takeWhile (not <$> isEndOfLine)
+        endOfLine
+
+    -- Arriving at first digit
+    -- skipSpace
+
+    -- Read all atoms
+    atoms' <- many1 $ gaussianParseXYZ
+
+    nrOfAtoms' <- parseNAtoms
+
+    let molecule' = Molecule {
+        _molName = "",
+        _nrOfAtoms = nrOfAtoms',
+        _atoms = atoms'
+    }
+
+    -- Find the SCF Done tag
+    _ <- manyTill anyChar (string "SCF Done:") <* skipSpace
+
+    -- ==> Read the method tag from the following brackets
+    method' <- string "E(" *> manyTill anyChar (string ")")
+    _ <- skipSpace *> string "=" *> skipSpace
+
+    -- Read the SCF energy
+    scfEnergy' <- double
+    -- _ <- manyTill anyChar endOfLine
+
+    -- Return the data to the GaussianInfo data type
+    return GaussianInfo {
+        _method     = method',
+        _scfEnergy  = scfEnergy',
+        _molecule   = Just molecule'
+    }
+
+
+parseNAtoms :: Parser Int
+parseNAtoms = manyTill anyChar (string "NAtoms=") *> skipSpace *> decimal
+
+
+gaussianFreqParser :: Parser [NormalMode]
+gaussianFreqParser = do
+
+    nrAtoms' <- parseNAtoms
+    _ <- manyTill anyChar (string "normal coordinates:")
+
+    -- traceShowM test'
+
+    _ <- takeWhile (not <$> isEndOfLine)
+    endOfLine
+
+    normalModes' <- count (nrAtoms' - 2) $ do
+        modeNumbers' <- count 3 $ do
+            skipSpace
+            decimal
+
+        traceShowM modeNumbers'
+
+        _ <- takeWhile (not <$> isEndOfLine)
+        endOfLine
+
+        symmetries' <- count 3 $ do
+            skipSpace                   -- Whitespaces before
+            manyTill anyChar space      -- Symmetry symbol
+
+        -- Skip until the next block
+        _ <- manyTill anyChar (string "--")
+
+        traceShowM symmetries'
+
+        frequencies' <- count 3 $ do
+            skipSpace
+            double                      -- Take the frequency
+
+        traceShowM frequencies'
+
+        _ <- manyTill anyChar (string "--")
+
+        reducedMass' <- count 3 $ do
+            skipSpace
+            double                      -- Take the reduced mass
+
+        traceShowM reducedMass'
+
+        _ <- manyTill anyChar (string "--")
+
+        forceConst' <- count 3 $ do
+            skipSpace
+            double                      -- Take the force constant
+
+        traceShowM forceConst'
+
+        _ <- manyTill anyChar (string "--")
+
+        irIntensity' <- count 3 $ do
+            skipSpace
+            double                      -- Take the IR intensity
+
+        traceShowM irIntensity'
+
+        _ <- takeWhile (not <$> isEndOfLine)
+        endOfLine
+
+        -- Skip header
+        _ <- takeWhile (not <$> isEndOfLine)
+        endOfLine
+
+        atoms' <- count nrAtoms' $ do
+            -- Parse atom displacements line by line
+            skipSpace
+            centerNum' <- decimal <* skipSpace
+            atomNum' <- decimal
+
+            -- traceShowM [centerNum', atomNum']
+
+            count 3 $ do
+                skipSpace
+                xcoord' <- double <* skipSpace
+                ycoord' <- double <* skipSpace
+                zcoord' <- double
+
+                return Atom {
+                    _centerNumber   = Just centerNum',
+                    _atomNumber     = atomNum',
+                    _coordinate     = CartesianCoord xcoord' ycoord' zcoord'
+                }
+
+        let atomsOrdered' = transpose atoms'
+
+        traceShowM $ length <$> atomsOrdered'
+
+        return [
+            NormalMode {
+                _frequency      = frequencies' !! ii,
+                _redMass        = Just $ reducedMass' !! ii,
+                _forceConst     = Just $ forceConst' !! ii,
+                _irIntensity    = Just $ irIntensity' !! ii,
+                _displacement   = atomsOrdered' !! ii
+            } | ii <- [0..2]]
+
+    traceShowM $ length <$> normalModes'
+
+    -- return normalModes'
+    return $ concat normalModes'
